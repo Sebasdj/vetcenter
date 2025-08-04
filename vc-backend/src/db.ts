bd7bd79550c6015ef3d26204745f0fba82f77d0d
Mariely Sanchez <20220223@miucateci.edu.do>
2025-08-04 16:45:36 -0400

    npm install y modificacion de db.ts

--- vc-backend/src/db.ts
+++ vc-backend/src/db.ts
@@ -4,21 +4,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const { 
-  USER,
-  USER_PASSWORD,
-  DATABASE,
-  HOST,
-  PORT
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT
 } = process.env
 
 const poolObj = {
-  user: USER,
-  host: HOST,
-  database: DATABASE,
-  password: USER_PASSWORD,
-  port: PORT ? Number.parseInt(PORT) : 5432
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT ? Number.parseInt(DB_PORT) : 5432
 }
 
 console.log(poolObj)
+console.log('Tipo de password:', typeof DB_PASSWORD)
 
-export const pool = new Pool(poolObj);
+export const pool = new Pool(poolObj)
